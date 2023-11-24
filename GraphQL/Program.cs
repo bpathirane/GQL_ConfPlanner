d4b809a542825e5b54b5c28cb02c989746993687
Buddhi Pathirane <2478087+bpathirane@users.noreply.github.com>
2023-11-23 20:36:40 -0500

    Added GraphQL Query, in running condition

--- GraphQL/Program.cs
+++ GraphQL/Program.cs
@@ -1,3 +1,4 @@
+using ConferencePlanner.GraphQL;
 using ConferencePlanner.GraphQL.Data;
 using Microsoft.EntityFrameworkCore;
 
@@ -5,9 +6,14 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlite("Data Source=conference.db"));
 
+builder.Services
+    .AddGraphQLServer()
+    .AddQueryType<Query>();
+
 var app = builder.Build();
 
-
 app.MapGet("/", () => "Hello World!");
+
+app.MapGraphQL();
 
 app.Run();
